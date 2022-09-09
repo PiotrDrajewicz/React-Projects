619d112d8b9b2c89daf5c6e2a973e396afdd5d7b
Piotr Drajewicz <drpiterek@gmail.com>
2022-09-09 20:46:34 +0200

    context setup done

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio repellat
+        distinctio quidem reprehenderit iusto, adipisci tempora harum
+        consequatur? Iusto, itaque optio quibusdam quasi in distinctio dolore
+        expedita, rerum ex aperiam cupiditate. Facilis quos alias placeat rem ab
+        officiis in optio?
+      </p>
+    </section>
+  );
+};
+
+export default About;
