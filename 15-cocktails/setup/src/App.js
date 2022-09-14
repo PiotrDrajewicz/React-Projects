98ad90e61af48bd9d1bb7de80d736032315467b8
Piotr Drajewicz <drpiterek@gmail.com>
2022-09-14 16:55:59 +0200

    details 2

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import pages
+//import pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SingleCocktail from "./pages/SingleCocktail";
 import Error from "./pages/Error";
-// import components
+//import components
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
